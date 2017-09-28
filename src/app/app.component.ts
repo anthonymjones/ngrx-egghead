d8a7f9c6361fd4f14eff8fbbd49e86d75c4b6116
Anthony Jones <anthony.mjones@gmail.com>
2017-09-28 16:41:51 -0600

    Completed Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs';
+import { Subject } from 'rxjs/Subject';
+import { Store } from '@ngrx/store';
+
+import { SECOND, HOUR, ADVANCE, RECALL } from './reducers/reducers';
 
 @Component({
   selector: 'ngrxe-root',
-  templateUrl: './app.component.html',
+  template: `
+    <input #inputNum type="number" value="0">
+    <button (click)="click$.next(inputNum.value)">Update</button>
+    <clock [time]="time | async | date:'hh:mm:ss a on EEEE, MMMM dd, yyyy'"></clock>
+
+    <div (click)="person$.next(person)" *ngFor="let person of people | async">
+      {{person.name}} is in {{person.time | date:'hh:mm:ss a'}}
+    </div>
+    <button (click)="recall$.next()">Recall</button>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngrxe';
+  click$ = new Subject()
+    .map((value: string) => ({ type: HOUR, payload: parseInt(value) }));
+
+  person$ = new Subject()
+    .map((value) => ({ payload: value, type: ADVANCE }));
+
+  recall$ = new Subject();
+
+  seconds$ = Observable
+    .interval(1000)
+    .mapTo({ type: SECOND, payload: 1 });
+
+  time;
+  people;
+
+  constructor(store: Store<any>) {
+    this.time = store.select('clock');
+    this.people = store.select('people');
+
+    Observable.merge(
+      this.click$,
+      this.seconds$,
+      this.person$,
+      this.recall$
+        .withLatestFrom(this.time, (_, y) => y)
+        .map((time) => ({ type: RECALL, payload: time }))
+    )
+      .subscribe(store.dispatch.bind(store));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { ClockComponent } from './components/clock.component';
+
+import { clock, people } from './reducers/reducers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClockComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ clock, people })
   ],
   providers: [],
   bootstrap: [AppComponent]
